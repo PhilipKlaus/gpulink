398017f7b9e87c42c00805d6991195dfbb23b7c6
Philip Klaus <philip.klaus@ait.ac.at>
2022-03-07 10:57:46 +0100

    Change recording to use the low level nvml binding

--- README.md
+++ README.md
@@ -2,15 +2,32 @@
 A tool for monitoring and displaying GPU stats
 
 ## Installation
-1. Create pipenv environment: `pipenv install`  
-2. Run recording `python gpu-link.py`
-3. Stop recording *Ctrl+C*
-4. *mem_consumption.png* is created in the current working directory
+1. Create a virtual python environment: `python -m venv gpu-link`
+2. Enter the environment: `.\gpu-link\Scripts\Activate`
+3. Install python packages: `pip install -r requirements.txt`
 
-## Example Output
+## Usage
 ```
-GPU[0]: min memory consumption: 2234.70703125[MB]  
-GPU[0]: max memory consumption: 1808.6171875[MB]
+usage: gpu-link.py [-h] [-o OUTPUT]
+
+GPU-Link memory tracing
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        Path to memory graph plot.
+```
+
+## Example
+1. Enter the environment: `.\gpu-link\Scripts\Activate`
+2. Run recording `python gpu-link.py -o memory_consumption.png`
+3. Stop recording *Ctrl+C*
+4. Plot is saved to `memory_consumption.png`.
+
+### Output
+```
+GPU[0]: max memory consumption: 2234.70703125[MB]  
+GPU[0]: min memory consumption: 1808.6171875[MB]
 ```
 ![Memory consumption over time](./docs/mem_consumption.png)
 
--- gpu-link.py
+++ gpu-link.py
@@ -1,53 +1,118 @@
+import argparse
 import signal
-import sys
 import time
-import numpy as np
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
 
-from pynvml.smi import nvidia_smi
 import matplotlib.pyplot as plt
-
-measurements = []
-sampling_interval = 0.01
-
-
-def get_mem_used(measurements):
-    amount = len(nvsmi.DeviceQuery('gpu_name,gpu_bus_id,vbios_version')["gpu"])
-    mem_consumed = [[] for gpu in range(amount)]
-
-    for entry in measurements:
-        measure = entry["gpu"]
-        for gpu, data in enumerate(measure):
-            mem_consumed[gpu].append(data["fb_memory_usage"]["total"] - data["fb_memory_usage"]["free"])
-
-    return mem_consumed
+import numpy as np
+from pynvml import *
 
 
-def generate_report(mem_consumed):
-    amount = len(mem_consumed[0])
-    x = np.arange(0, amount * sampling_interval, sampling_interval)
-    for i, data in enumerate(mem_consumed):
-        plt.plot(x, data, label=f"GPU[{i}]")
-    plt.title("Gpu memory usage [MB]")
+@dataclass
+class GPUMemRecord:
+    """
+    Stores the memory usage for a single GPU device.
+    """
+    timestamps: np.ndarray
+    total: np.ndarray
+    used: np.ndarray
+    free: np.ndarray
+
+
+class NVContext:
+    """
+    Context for wrapping nvmlInit() and nvmlShutdown().
+    """
+
+    def __enter__(self):
+        nvmlInit()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        nvmlShutdown()
+
+
+class NVRecorder:
+    """
+    Manages GPU memory recording. Recordings must be performed in context of a NVContext.
+    """
+
+    def __init__(self, gpus: int):
+        self._handles = []
+        self.memory_records = [GPUMemRecord(np.array([]), np.array([]), np.array([]), np.array([])) for _ in
+                               range(gpus)]
+        for i in range(gpus):
+            self._handles.append(nvmlDeviceGetHandleByIndex(i))
+
+    def _execute_query(self, query, *args, **kwargs):
+        res = []
+        for handle in self._handles:
+            timestamp = time.time_ns()
+            res.append((timestamp, query(handle, *args, **kwargs)))
+        return res
+
+    def rec_memory_info(self) -> None:
+        """
+        Records the actual Memory usage in all GPU devices.
+        :return: None
+        """
+        res = self._execute_query(nvmlDeviceGetMemoryInfo)
+        for i, rec in enumerate(res):
+            self.memory_records[i].timestamps = np.append(self.memory_records[i].timestamps, rec[0])
+            self.memory_records[i].total = np.append(self.memory_records[i].total, rec[1].total)
+            self.memory_records[i].used = np.append(self.memory_records[i].used, rec[1].used)
+            self.memory_records[i].free = np.append(self.memory_records[i].free, rec[1].free)
+
+
+def generate_memory_graph(img_path: Path, memory_records: List[GPUMemRecord]) -> None:
+    """
+    Generates and saves a plot of the GPU memory usage over time
+    :param img_path: The path to the image file.
+    :param memory_records: The memory_records to be plotted.
+    :return: None
+    """
+    mb = 1e6
+    sec = 1e9
+
+    for i, rec_set in enumerate(memory_records):
+        timestamps = (rec_set.timestamps - rec_set.timestamps[0]) / sec
+        mem_usage = rec_set.used / mb
+        plt.plot(timestamps, mem_usage, label=f"GPU[{i}]")
+
     plt.legend(loc="upper left")
-    plt.ylabel("Memory [MB]")
+    plt.ylabel("Memory used [MB]")
     plt.xlabel("Time [s]")
-    plt.savefig("mem_consumption.png")
+    plt.savefig(img_path.as_posix())
 
 
-def signal_handler(_, __):
-    mem_consumed = get_mem_used(measurements)
-    mem_max = [max(m) for m in mem_consumed]
-    mem_min = [min(m) for m in mem_consumed]
-    generate_report(mem_consumed)
-    for i, mem in enumerate(zip(mem_min, mem_max)):
-        print(f"GPU[{i}]: min memory consumption: {mem[1]}[MB]")
-        print(f"GPU[{i}]: max memory consumption: {mem[0]}[MB]")
-    sys.exit(0)
+def _signal_handler(_, __):
+    global _should_run
+    _should_run = False
 
+
+def _check_arguments(arguments):
+    supported_file_types = plt.gcf().canvas.get_supported_filetypes()
+    if args.output and arguments.output.suffix[1:] not in supported_file_types:
+        raise ValueError(f"Output format '{arguments.output.suffix}' not supported")
+
+
+_should_run = True
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-    nvsmi = nvidia_smi.getInstance()
-    while True:
-        measurements.append(nvsmi.DeviceQuery('memory.free, memory.total'))
-        time.sleep(sampling_interval)
+    parser = argparse.ArgumentParser(description="GPU-Link memory tracing")
+    parser.add_argument("-o", "--output", type=Path, default=None,
+                        help="Path to memory graph plot.")
+    args = parser.parse_args()
+
+    _check_arguments(args)
+
+    signal.signal(signal.SIGINT, _signal_handler)
+    with NVContext():
+        deviceCount = nvmlDeviceGetCount()
+        recorder = NVRecorder(deviceCount)
+        while _should_run:
+            recorder.rec_memory_info()
+
+    if args.output:
+        generate_memory_graph(args.output, recorder.memory_records)
